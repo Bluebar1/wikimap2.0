841a8f7c21778db3a4d8412036060ff39b13131d
Nick Blaauboer <yupnick123@gmail.com>
2020-04-12 19:14:58 -0400

    added back and help buttons

---
+++ lib/modules/animated_help.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class Help extends StatefulWidget {
+  _Help createState() => _Help();
+}
+
+class _Help extends State<Help> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    scaleAnimation =
+        CurvedAnimation(parent: controller, curve: Curves.elasticInOut);
+    controller.addListener(() {
+      setState(() {});
+    });
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Material(
+        color: Colors.transparent,
+        child: ScaleTransition(
+          scale: scaleAnimation,
+          child: Container(
+            decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0))),
+            child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 300,
+                    ),
+                    Text('HELP PAGE'),
+                    Text('HELP PAGE'),
+                    Text('HELP PAGE'),
+                    Text('HELP PAGE'),
+                  ],
+                )),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/wiki_map_view.dart
+++ lib/screens/wiki_map_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:wiki_map/modules/animated_help.dart';
 import 'package:wiki_map/providers/geosearch_provider.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:wiki_map/providers/swiper_index_provider.dart';
@@ -24,24 +25,81 @@ class WikiMapView extends StatelessWidget {
   Widget build(BuildContext context) {
     var geoSearchProvider = Provider.of<GeoSearchProvider>(context);
     var swiperIndexProvider = Provider.of<SwiperIndexProvider>(context);
-    return GoogleMap(
-      initialCameraPosition: CameraPosition(
-          target: LatLng(geoSearchProvider.startingPosition.latitude,
-              geoSearchProvider.startingPosition.longitude),
-          zoom: 16.0),
-      zoomGesturesEnabled: true,
-      markers: Set<Marker>.of(geoSearchProvider.currentMarkers.toList()),
-      onLongPress: (position) {
-        geoSearchProvider.getResults(Position(
-            latitude: position.latitude, longitude: position.longitude));
-        swiperIndexProvider.changeCurrentIndex(0);
-      },
-      onMapCreated: (controller) =>
-          geoSearchProvider.createController(controller),
+    return Scaffold(
+      floatingActionButton: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 50, 0, 0),
+            child: Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: FloatingActionButton(
+                    backgroundColor: Colors.grey,
+                    foregroundColor: Colors.black,
+                    heroTag: "backbutton",
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      print('back button pressed');
+                    },
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      size: 30,
+                    ),
+                  ),
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+            child: Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: FloatingActionButton(
+                    backgroundColor: Colors.grey,
+                    foregroundColor: Colors.black,
+                    heroTag: "helpbutton",
+                    onPressed: () {
+                      showDialog(context: context, builder: (_) => Help());
+                      print('help button pressed');
+                    },
+                    child: Icon(
+                      Icons.help_outline,
+                      size: 40,
+                      color: Colors.black,
+                    ),
+                  ),
+                )),
+          )
+        ],
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+            target: LatLng(geoSearchProvider.startingPosition.latitude,
+                geoSearchProvider.startingPosition.longitude),
+            zoom: 16.0),
+        zoomGesturesEnabled: true,
+        markers: Set<Marker>.of(geoSearchProvider.currentMarkers.toList()),
+        onLongPress: (position) {
+          geoSearchProvider.getResults(Position(
+              latitude: position.latitude, longitude: position.longitude));
+          swiperIndexProvider.changeCurrentIndex(0);
+        },
+        onMapCreated: (controller) =>
+            geoSearchProvider.createController(controller),
 
-      onCameraMove: (position) =>
-          geoSearchProvider.changeCurrentCameraPosition(position),
-      // geoSearchProvider.controller.complete(controller),
+        onCameraMove: (position) =>
+            geoSearchProvider.changeCurrentCameraPosition(position),
+        // geoSearchProvider.controller.complete(controller),
+      ),
     );
   }
 }
